9ec340062330072490d7229e5a13ab6d0b6fca63
Jinyamyzk <ddjinya@gmail.com>
2020-05-19 21:24:41 +0900

    add tokenize

--- test.py
+++ test.py
@@ -10,12 +10,11 @@ data_dir_path = Path('.')
 corpus_dir_path = Path('.')
 
 
-file_name = 'syllabus.csv'
+file_name = 'syllabus_sample.csv'
 
 with open(data_dir_path.joinpath(file_name), 'r', encoding='utf-8') as file:
     next(file)
     texts = file.readlines()
-
 texts = [text_.replace('\n', '') for text_ in texts]
 
 # janomeのAnalyzerを使うことで、文の分割と単語の正規化をまとめて行うことができる
@@ -72,37 +71,19 @@ analyzer = Analyzer(char_filters, tokenizer, token_filters)
 
 tokens_list = []
 raw_texts = []
+
 for text in texts:
     # 文を分割し、単語をそれぞれ正規化する
     text_ = [token.base_form for token in analyzer.analyze(text)]
     if len(text_) > 0:
         tokens_list.append([token.base_form for token in analyzer.analyze(text)])
         raw_texts.append(text)
-
-# 正規化された際に一文字もない文の削除後の元テキストデータ
 raw_texts = [text_+'\n' for text_ in raw_texts]
 with open(data_dir_path.joinpath(file_name.replace('.txt', '_cut.txt')), 'w', encoding='utf-8') as file:
     file.writelines(raw_texts)
-
-# 単語リストの作成
 words = []
 for text in tokens_list:
     words.extend([word+'\n' for word in text if word != ''])
 with open(corpus_dir_path.joinpath(file_name.replace('.txt', '_word_list.txt')), 'w', encoding='utf-8') as file:
     file.writelines(words)
-
-# 単語リストからストップワードを削除
-## ストップワードファイルからの呼び込み
-stop_words = []
-path = 'stopwords_jp.txt'
-with open(path) as f:
-    stop_words = f.readlines()
-
-## ストップワードの除外
-changed_words = [word for word in words if word not in stop_words]
-print('-----------------')
-print('Delited ' + str(len(words) - len(changed_words)) + ' words' )
-print('-----------------')
-
-with open(corpus_dir_path.joinpath(file_name.replace('.csv', '_word_list_exclude.csv')), 'w', encoding='utf-8') as file:
-    file.writelines(changed_words)
+print(words)
