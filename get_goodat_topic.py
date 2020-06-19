58e79031767756e74f709c51b6a084a15ede2c16
Jinyamyzk <ddjinya@gmail.com>
2020-06-19 21:24:08 +0900

    Change get_goddat.py

--- get_goodat_topic.py
+++ get_goodat_topic.py
@@ -14,6 +14,23 @@ def search_goodat_topic(nendo, code, grade):
         topic_grades = [n * grade for n in topic_value[0]]
     return topic_grades
 
+def get_reccomend_class(nendo,topic_index):
+    available_classes = df[(df['年度'] == nendo)]
+    class_names_topics = available_classes[['科目名','トピックの確率']].values.tolist()
+    for i in class_names_topics:
+        i[1] = i[1][topic_index]
+        class_names.append(i)
+
+
+
+
+
+
+
+
+
+
+
 
 df = pd.read_json('syllabus.json')
 with open('grade.csv') as f:
@@ -23,9 +40,15 @@ with open('grade.csv') as f:
     f.close()
 
 for row in grades:
-    print(int(row[0]), row[1], float(row[2]))
     topic_grades = search_goodat_topic(int(row[0]), row[1], float(row[2]))
     if topic_grades is not None:
         sum_topic_odds = [topic_grades[i] + sum_topic_odds[i] for i in range(len(topic_grades))]
 
-print(sum_topic_odds)
+topic_index = sum_topic_odds.index(max(sum_topic_odds))
+
+class_names = []
+
+get_reccomend_class(2020,topic_index)
+class_names_sorted = sorted(class_names, reverse=True, key=lambda x: x[1])
+for i in class_names_sorted[0:5]:
+    print(i[0])
