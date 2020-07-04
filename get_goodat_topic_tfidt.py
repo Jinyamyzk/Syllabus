7a79bfbdcd80a11d8b99ee177298bcc1ec56f919
Jinyamyzk <ddjinya@gmail.com>
2020-07-04 14:16:31 +0900

    Add thidf

---
+++ get_goodat_topic_tfidt.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import pickle
+import csv
+import json
+
+sum_topic_odds = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+def search_goodat_topic(nendo, code, grade):
+    topic_grades = None
+    taken_class = df[(df['年度'] == nendo) & (df['時間割コード'] == code)]
+    topic_value = taken_class['トピックの確率'].values.tolist()
+    if len(topic_value) == 1:
+        topic_grades = [n * grade for n in topic_value[0]]
+    return topic_grades
+
+def get_reccomend_class(nendo,topic_index):
+    available_classes = df[(df['年度'] == nendo)]
+    class_names_topics = available_classes[['科目名','トピックの確率']].values.tolist()
+    for i in class_names_topics:
+        i[1] = i[1][topic_index]
+        class_names.append(i)
+
+
+
+
+
+
+
+
+
+
+
+
+df = pd.read_json('syllabus_tfidf.json')
+with open('grade.csv') as f:
+    h = next(csv.reader(f))
+    reader = csv.reader(f)
+    grades = [e for e in reader]
+    f.close()
+
+for row in grades:
+    topic_grades = search_goodat_topic(int(row[0]), row[1], float(row[2]))
+    if topic_grades is not None:
+        sum_topic_odds = [topic_grades[i] + sum_topic_odds[i] for i in range(len(topic_grades))]
+
+topic_index = sum_topic_odds.index(max(sum_topic_odds))
+
+class_names = []
+
+get_reccomend_class(2020,topic_index)
+class_names_sorted = sorted(class_names, reverse=True, key=lambda x: x[1])
+for i in class_names_sorted[0:5]:
+    print(i[0])
--- lda_tfidf.py
+++ lda_tfidf.py
@@ -42,36 +42,60 @@ tfidf = gensim.models.TfidfModel(corpus)
 # make corpus_tfidf
 corpus_tfidf = tfidf[corpus]
 
+NUM_TOPICS = 9
+
 # LDA Model
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=9, alpha='symmetric', random_state=0)
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=NUM_TOPICS, alpha='symmetric', random_state=0)
 
+# test
+N = sum(count for doc in corpus for id, count in doc)
+print("N: ",N)
 
+perplexity = np.exp2(-lda_model.log_perplexity(corpus))
+print("perplexity:", perplexity)
+
+# テストデータをモデルに掛ける
+test_corpus = [dictionary.doc2bow(text) for text in texts]
+
+topic_results = []
+# クラスタリング結果を出力
+for unseen_doc in test_corpus:
+    score_by_topic = [0] * NUM_TOPICS
+    for topic, score in lda_model[unseen_doc]:
+        score_by_topic[topic] = score
+    topic_results.append(score_by_topic)
+
+df = pd.read_csv('syllabus.csv')
+df['トピックの確率'] = topic_results
+print(df)
+df.to_json('syllabus_tfidf.json')
 
 # WordCloud
-fig, axs = plt.subplots(ncols=2, nrows=math.ceil(lda_model.num_topics/2), figsize=(16,20))
-axs = axs.flatten()
-
-def color_func(word, font_size, position, orientation, random_state, font_path):
-    return 'darkturquoise'
-
-for i, t in enumerate(range(lda_model.num_topics)):
-
-    x = dict(lda_model.show_topic(t, 30))
-    im = WordCloud(
-        background_color='black',
-        color_func=color_func,
-        max_words=4000,
-        width=300, height=300,
-        random_state=0
-    ).generate_from_frequencies(x)
-    axs[i].imshow(im.recolor(colormap= 'Paired_r' , random_state=244), alpha=0.98)
-    axs[i].axis('off')
-    axs[i].set_title('Topic '+str(t))
-
-# vis
-plt.tight_layout()
-plt.show()
-
-# save as png
-plt.savefig('wordcloud.png')
+# fig, axs = plt.subplots(ncols=2, nrows=math.ceil(lda_model.num_topics/2), figsize=(16,20))
+# axs = axs.flatten()
+#
+# def color_func(word, font_size, position, orientation, random_state, font_path):
+#     return 'darkturquoise'
+#
+# for i, t in enumerate(range(lda_model.num_topics)):
+#
+#     x = dict(lda_model.show_topic(t, 30))
+#     im = WordCloud(
+#         background_color='black',
+#         font_path="/Library/Fonts//ヒラギノ丸ゴ ProN W4.ttc",
+#         color_func=color_func,
+#         max_words=4000,
+#         width=300, height=300,
+#         random_state=0
+#     ).generate_from_frequencies(x)
+#     axs[i].imshow(im.recolor(colormap= 'Paired_r' , random_state=244), alpha=0.98)
+#     axs[i].axis('off')
+#     axs[i].set_title('Topic '+str(t))
+#
+# # vis
+# plt.tight_layout()
+# plt.show()
+#
+# # save as png
+# plt.savefig('wordcloud.png')
